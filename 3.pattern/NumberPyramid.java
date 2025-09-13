0dab638eab4bd002dbf560a893cd3037e048132e
kumar <rahuldolai004@gmail.com>
2025-09-14 00:54:00 +0530

    Add Numbeer pyramid pattern

--- 3.pattern/NumberPyramid.java
+++ 3.pattern/NumberPyramid.java
@@ -1,3 +1,19 @@
 public class NumberPyramid {
-    
+    public static void main(String[] args) {
+        int num = 5;
+        int count = 1;
+        for (int i = 0; i < num; i++) {
+            // spaces
+            for (int j = 0; j < num - i + 1; j++) {
+                System.out.print(" ");
+            }
+            // count
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print(count + " ");
+            }
+            count++;
+            System.out.println("");
+
+        }
+    }
 }
