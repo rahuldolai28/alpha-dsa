a343b300604d0f0ee5e1e1f552e87c98d34f9575
kumar <rahuldolai004@gmail.com>
2025-09-14 18:54:37 +0530

    Add PrefixSum.java

--- 5.arrays/PrefixSum.java
+++ 5.arrays/PrefixSum.java
@@ -1,29 +1,39 @@
 public class PrefixSum {
 
-    public static void subArraySum(int num[]){
+    public static void subArraySum(int num[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[num.length];
+
+        prefix[0] = num[0];
+        // calculate prefix array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + num[i];
+        }
 
         for (int i = 0; i < num.length; i++) {
             int start = i;
             for (int j = i; j < num.length; j++) {
                 int end = j;
-                currSum = 0;
-                for(int k = start; k<=end;k++){
-                    //subarray sum
-                    currSum += num[k];
-                }
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+                // for(int k = start; k<=end;k++){
+                // //subarray sum
+                // currSum += num[k];
+                // }
+
                 System.out.println(currSum);
-                if (maxSum<currSum) {
+                if (maxSum < currSum) {
                     maxSum = currSum;
                 }
             }
 
         }
-        System.out.println("max sum = "+maxSum);
+        System.out.println("max sum = " + maxSum);
     }
+
     public static void main(String[] args) {
-         int num[] = { 2, 4, 5, 8 };
+        int num[] = { 2, 4, 5, 8 };
         subArraySum(num);
     }
 }
