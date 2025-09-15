5ce40fe42e01d978ac5fc0190ffd232d59dd37f2
kumar <rahuldolai004@gmail.com>
2025-09-15 11:14:51 +0530

    bubble sort with better time complexity for best case

--- 6.BasicSorting/BubbleSort.java
+++ 6.BasicSorting/BubbleSort.java
@@ -1,24 +1,33 @@
-public class BubbleSort{
-    public static void sort(int arr[]){
-        for (int turn = 0; turn < arr.length-1; turn++) {
-            for (int i = 0; i < arr.length-1-turn; i++) {
-                if(arr[i]>arr[i+1]){
-                    //swap
+public class BubbleSort {
+    public static void sort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            boolean sorted = true;
+            for (int i = 0; i < arr.length - 1 - turn; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    // swap
                     int temp = arr[i];
-                    arr[i]= arr[i+1];
-                    arr[i+1]= temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    sorted = false;
                 }
+            }
+            // if no swap -> already sorted
+            if (sorted) {
+                System.out.println("Already sorted");
+                break;
             }
         }
     }
-    public static void printArr(int arr[]){
+
+    public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
 
     }
+
     public static void main(String[] args) {
-        int arr[]={5,2,8,3,9,1};
+        int arr[] = { 5, 6, 12 };
         sort(arr);
         printArr(arr);
 
