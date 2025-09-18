f6b4fb83ea54cb9d91874f57ed316aab16d9fde2
kumar <rahuldolai004@gmail.com>
2025-09-18 23:40:08 +0530

    abstract

---
+++ 10.Oops/Abstract.java
@@ -0,0 +1,46 @@
+public class Abstract {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        Chicken c = new Chicken();
+        h.eat();
+        h.walk();
+        c.eat();
+        c.walk();
+        h.ChangeColor();
+        System.out.println(h.color);
+        System.out.println(c.color);
+
+
+        // Can't create
+        // Animal a = new Animal();
+    }
+}
+
+abstract class Animal {
+    String color;
+
+    Animal() {
+        color = "brown";
+    }
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void walk() {
+        System.out.println("Walks on 4 legs");
+    }
+    void ChangeColor(){
+        color = "dark brown";
+    }
+}
+
+class Chicken extends Animal {
+    void walk() {
+        System.out.println("Walks on 2 legs");
+    }
+}
