87ab57f8ce3d2f7eb93fa246524ba70ff7dcf3ef
kumar <rahuldolai004@gmail.com>
2026-03-29 09:12:41 +0530

    learned merge sort

---
+++ 12.Divide And Conquer/MergeSort.java
@@ -0,0 +1,65 @@
+public class MergeSort {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si, int ei) {
+        // base case
+        if (si >= ei) {
+            return;
+        }
+        // kaam
+        int mid = si + (ei - si) / 2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si; // iterotor for left part;
+        int j = mid + 1; // iteretor for right part;
+        int k = 0; // iterortor for temp arr
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left part
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right part
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // copy temp to original arr
+        for (k = 0, i = si; k < temp.length; k++, i++)
+            arr[i] = temp[k];
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        System.out.print("Initial Array: ");
+        printArr(arr);
+
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.print("After sorting Array: ");
+
+        printArr(arr);
+
+    }
+}
