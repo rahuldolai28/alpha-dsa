4d4fc042f36fef8fe2e4d2aa4b0eb49c5813323e
kumar <rahuldolai004@gmail.com>
2026-08-13 23:40:07 +0530

    previous sessions

---
+++ 22.BT3/LowestCommonAncestor.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+
+public class LowestCommonAncestor {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // approcah 1 = path based approach
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i).data != path2.get(i).data) {
+                break;
+            }
+        }
+        System.out.println(path1);
+        System.out.println(path2);
+        // last equal node -> i-1th
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
+
+    // Approach 2
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        // let, (leftLCA = val rightLCA = null) -> ans =leftlca
+        if (leftLca == null) {
+            return rightLca;
+        }
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+        /*
+         * 1
+         * / \
+         * 2 3
+         * /\ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 7, n2 = 6;
+
+        Node ans = lca(root, n1, n2);
+        System.out.println(ans.data);
+    }
+}
